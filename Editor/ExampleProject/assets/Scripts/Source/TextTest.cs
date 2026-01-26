aaceae2a4bb5bf9749bda1a0ee8c1e3ba0aa1ae6
Andthehand <sixdefords50@gmail.com>
2026-01-25 23:04:47 -0500

    TextComponent partly working in C#

---
+++ Editor/ExampleProject/assets/Scripts/Source/TextTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using RealEngine;
+
+public class TextTest : Entity {
+
+    void OnCreate() {
+        Logger.Warn($"ExampleClass2 Entity Id is : {ID}");
+        unsafe {
+            TextRendererComponent* textRenderer = GetComponent<TextRendererComponent>();
+            textRenderer->Color = new Vector4(255.0f, 0.0f, 0.0f, 255.0f);
+        }
+    }
+
+    public static void StaticMethod(float value) {
+        Console.WriteLine($"StaticMethod: {value}");
+    }
+}
--- RealEngine-ScriptCore/Source/RealEngine/Components/TextRendererComponent.cs
+++ RealEngine-ScriptCore/Source/RealEngine/Components/TextRendererComponent.cs
@@ -1,7 +1,29 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Drawing;
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 namespace RealEngine {
+    [StructLayout(LayoutKind.Sequential)]
     public struct TextRendererComponent {
+        // 40 Byte padding
+        private long _A;
+        private long _B;
+        private long _C; 
+        private long _D;  
+        private long _E;  
+
+        private Vector4 color;
+        public Vector4 Color
+        {
+            get
+            {
+                return color * 255f;
+            }
+            set
+            {
+                color = value / 255f;
+            }
+        }
     }
 }
