23466c3965373709f22540ef60619a550ee7e647
Andthehand <sixdefords50@gmail.com>
2026-01-28 16:54:43 -0500

    Now showing renderstats

--- Editor/src/Panels/StatusPanel.cpp
+++ Editor/src/Panels/StatusPanel.cpp
@@ -4,8 +4,31 @@
 
 namespace RealEngine {
 	void StatusPanel::OnImGui() {
+		RE_PROFILE_FUNCTION();
 		if (ImGui::Begin("Status Bar")) {
 			ImGui::Text("FPS: %.3f", ImGui::GetIO().Framerate);
+
+			if(ImGui::TreeNode("Render Stats")) {
+				static const RenderStats& renderStats = Project::GetCurrentScene()->GetRenderStats();
+				ImGui::Text("Draw Calls: %d", renderStats.DrawCalls);
+				
+				ImGui::Separator();
+				
+				// 2D
+				ImGui::Text("2D:");
+				ImGui::Text("  Draw Calls: %d", renderStats.Sprites.DrawCalls);
+				ImGui::Text("  Quads: %d", renderStats.Sprites.QuadCount);
+				ImGui::Text("  Texture Binds: %d", renderStats.Sprites.TextureBinds);
+				
+				ImGui::Separator();
+
+				// Text
+				ImGui::Text("Text:");
+				ImGui::Text("  Draw Calls: %d", renderStats.Text.DrawCalls);
+				ImGui::Text("  Glyphs: %d", renderStats.Text.GlyphCount);
+
+				ImGui::TreePop();
+			}
 		}
 
 		ImGui::End();
--- Editor/src/Panels/StatusPanel.h
+++ Editor/src/Panels/StatusPanel.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "RealEngine.h"
 
 #include "Panel.h"
 
